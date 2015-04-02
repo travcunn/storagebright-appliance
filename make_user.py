6e9876f4bd87d5b89982786439d162197288a694
Travis Cunningham <tcunningham@smartfile.com>
2015-04-02 15:00:34 -0400

    Updated forms for authentication against remote servers.

--- make_user.py
+++ make_user.py
@@ -2,9 +2,17 @@ from __future__ import absolute_import
 from app import db
 from app.models import User
 
+USERNAME = 'david@storagebright.com'
+PASSWORD = 'test'
 
 u = User()
-u.email = 'test@google.com'
-u.set_password('test')
+u.email = USERNAME
+u.set_password(PASSWORD)
 db.session.add(u)
 db.session.commit()
+
+print("")
+print("#### Created new user ####")
+print("username: {}".format(USERNAME))
+print("password: {}".format(PASSWORD))
+print("##########################")
